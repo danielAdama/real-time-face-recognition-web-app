039bd4af59766a0cee20c566cc9194b082087986
danielAdama <adamadaniel321@gmail.com>
2023-01-25 18:22:28 +0100

    Seed initial test data

--- face_database.py
+++ face_database.py
@@ -3,29 +3,47 @@ import pickle
 import face_recognition
 import cv2
 import os
+import time
+import datetime
 from config import config
+import pymongo
+import json
 
+client = pymongo.MongoClient(config.CONNECTIONSTRING)
+db = client[config.DATABASE]
+tbl = db["UserEncoding"]
 
-knownEncodings = []
-knownNames = []
+output = {}
+counter = 0
 
-for name in os.listdir(config.KNOWN_PATH):
-    for filename in os.listdir(os.path.join(config.KNOWN_PATH,name)):
-        print(f"Processing {name}'s face!")
-        image = cv2.imread(os.path.join(config.KNOWN_PATH,name,filename))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+try:
+    for name in os.listdir(config.KNOWN_PATH):
+        for filename in os.listdir(os.path.join(config.KNOWN_PATH,name)):
+            counter += 1
+            #print(f"Processing {name}'s face!")
+            image = cv2.imread(os.path.join(config.KNOWN_PATH,name,filename))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            boxes = face_recognition.face_locations(image, model=config.MODEL) #cnn
 
-        # Detect the (x, y) co-ordinate of face(s)
-        boxes = face_recognition.face_locations(image, model=config.MODEL) #cnn
-
-        encodings = face_recognition.face_encodings(image, boxes)
-        # Loop over the encodings and add their name(s) and face(s), respectively to the database
-        for encoding in encodings:
-            knownEncodings.append(encoding)
-            knownNames.append(name)
-
-print('\nSuccessfully Serialized Face(s) into the Database'+"..."*3)
-data = {"encodings": knownEncodings, "names": knownNames}
-with open("encodings", "wb") as f:
-    f.write(pickle.dumps(data))
-    f.close()
+            if len(boxes) > 0:
+                encodings = face_recognition.face_encodings(image, boxes)
+                # Reset the counter for new faces
+                if (output.get('Name') is not None):
+                    if (output['Name'] != name):
+                        counter = 1
+                # Loop over the encodings and add their name(s) and face(s), respectively to the database
+                # for encoding in encodings:
+                for i in range(len(encodings)):
+                    data = {
+                        "Name":name,
+                        "FaceId": counter,
+                        "TimeCreated":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                        "Encoding": json.dumps(encodings[i].tolist())
+                    }
+                    db.UserEncoding.insert_one(
+                        data
+                    )
+                    output['Name'] = name
+    print('\nSuccessfully Serialized Face(s) into the Database'+"..."*3)
+except:
+    pass
