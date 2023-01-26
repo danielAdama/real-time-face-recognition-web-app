8629c57f8e379a48084f6779439d38bf5b8c8940
danielAdama <adamadaniel321@gmail.com>
2023-01-26 11:49:38 +0100

    Read from Db

--- Utilities/db.py
+++ Utilities/db.py
@@ -1,5 +1,7 @@
-
+import numpy as np
 import pymongo
+import ast
+
 
 class Database:
     def __init__(self, connectionString, databaseName, collection):
@@ -7,6 +9,7 @@ class Database:
         self.databaseName = databaseName
         self.collection = collection
         self.client = pymongo.MongoClient(self.connectionString)
+        self.face = {}
     
     def connected(self):
         try:
@@ -15,3 +18,10 @@ class Database:
         except:
             pass
         return self.db
+    
+    def processed_data(self):
+        cursor = self.connected().UserEncoding.find({}, {"_id":0})
+        data = [document for document in cursor]
+        self.face["names"] = [i["Name"] for i in data]
+        self.face["encodingArr"] = [np.array(ast.literal_eval(field["Encoding"])) for field in data]
+        return self.face
