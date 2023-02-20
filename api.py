bb63df31b7081e62a6f0a464cfc2600594fcc83c
danielAdama <adamadaniel321@gmail.com>
2023-02-20 19:32:38 +0100

    Replace Database with MongoDB

--- api.py
+++ api.py
@@ -1,15 +1,18 @@
 from quart import Quart, request, jsonify
 from quart_cors import cors
 import numpy as np
-import pickle
+import face_recognition
+import datetime
 from Utilities import process
 from face_verification.face_verify import FaceRecognition
 import cv2
 import imutils
 import logging
-import time
+from Utilities.db import Database
+import json
 import argparse
 from config import config
+
 
 app = Quart(__name__)
 app.config['Flask_DEBUG'] = "development"
@@ -18,8 +21,10 @@ cors(app)
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.NOTSET, datefmt='%d-%b-%y %H:%M:%S')
 _logger = logging.getLogger(handle)
 
-with open('encodings', 'rb') as f:
-    data = pickle.load(f)
+db = Database(config.CONNECTIONSTRING, config.DATABASE, "UserEncoding")
+data = db.processed_data()
+faceRecog = FaceRecognition(data=data, model='hog')
+
 
 @app.route('/api/v1/verify', methods = ["POST"])
 async def verify():
@@ -39,13 +44,12 @@ async def verify():
             
             frame = process.readb64(json_data)
             rgb = cv2.cvtColor(frame, config.COLOR)
-            rgb = imutils.resize(frame, 648)
+            rgb = imutils.resize(frame, 440)
             (h, w) = frame.shape[:2]
             r = w / rgb.shape[1]
             
-            fv = FaceRecognition(rgb, data=data, model='hog')
             try:
-                boxes, names, accs, encodings = fv.faceAuth()
+                boxes, names, accs, encodings = faceRecog.faceAuth(rgb)
                 if len(encodings) != 0:
                     for ((top, right, bottom, left), name) in zip(boxes, names):
                         top, right, bottom, left = (int(top*r)), (int(right*r)), (int(bottom*r)), (int(left*r))
@@ -98,6 +102,53 @@ async def verify():
                         },
                 "Error":"The data is not in json format",
             }, config.HTTP_404_NOT_FOUND
+
+@app.route('/api/v1/train', methods=["POST"])
+async def train():
+    json_data = await request.get_json()
+    try:
+        if not json_data:
+            _logger.warning("API VERIFY > please connect your camera")
+            return {
+                "BaseResponse":{
+                        "Status":False,
+                        "Message": "Please make sure your camera is ON",
+                        "Data": None,
+                    },
+                "Error": "Bad request"
+            }, config.HTTP_400_BAD_REQUEST
+
+        frame = process.readb64(json_data['image'])
+        boxes = face_recognition.face_locations(frame, model="hog")
+        encoding = face_recognition.face_encodings(frame, boxes)[0]
+        db.connected().UserEncoding.insert_one({
+            "Name":json_data['name'],
+            "TimeCreated":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "Encoding": json.dumps(encoding.tolist())
+        })
+        return {
+            "BaseResponse":{
+                "Status":True,
+                "Messsage":"Operation successfully"
+            },
+            "Detections": {
+                "Name":json_data['name'],
+                "Image":json_data['image'],
+                "TimeCreated":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                ##"Encoding": json.dumps(encoding)
+            },
+        }, config.HTTP_200_OK
+
+    except Exception as ex:
+        _logger.warning(f"API VERIFY > APPLICATION ERROR while recognizing face")
+        return {
+            "BaseResponse":{
+                        "Status":False,
+                        "Message":str(ex)
+                    },
+            "Error":"Something went wrong",
+        }, config.HTTP_500_INTERNAL_SERVER_ERROR
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Face Recognizer Api exposing hog model")
